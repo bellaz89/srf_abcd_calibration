6c3ebfcfd47b4f6cbbd1a6b2452e4eca8d6bc653
Andrea Bellandi <andrea.bellandi@desy.de>
2024-08-05 10:19:51 +0200

    Added LICENSE README and python project file

--- srf_abcd_calibration/find_bwdet.py
+++ srf_abcd_calibration/find_bwdet.py
@@ -1,3 +1,20 @@
+# SRF ABCD calibration program for superconducting accelerating cavity's
+# LLRF system
+# Copyright (C) 2024 Andrea Bellandi
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licens/>.
+
 import numpy as np
 from scipy.signal import savgol_filter
 import warnings
